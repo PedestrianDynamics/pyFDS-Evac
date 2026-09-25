5f6e7773c517f5332d64f66542df9eab54f711f2
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2026-09-25 14:43:57 +0200

    talks: credit Station floor plan to Fahy et al.
    The floor plan on slide 3 is from Fahy, Proulx and Flynn, Fire Safety
    Science 10 (2011) 197-209, not from NIST NCSTAR 2.

---
+++ talks/visibility-seminar-2026/assets/slidev/default-znj5M7Xz.js
@@ -0,0 +1,1 @@
+import{B as e,U as t,x as n}from"../modules/shiki-BbuZ3y2B.js";import{U as r}from"../index-8JEPfiA_.js";var i={},a={class:`slidev-layout default`};function o(r,i){return e(),n(`div`,a,[t(r.$slots,`default`)])}var s=r(i,[[`render`,o]]);export{s as t};
